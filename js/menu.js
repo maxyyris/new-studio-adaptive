9834917d4374dc363050c9235324521152335812
Maks <mla960800@gmail.com>
2024-05-23 18:29:32 +0200

    Update menu.js

--- js/menu.js
+++ js/menu.js
@@ -1,16 +1,23 @@
 (() => {
   const refs = {
-    openMenuBtn: document.querySelector("[data-menu-open]"),
-    closeMenuBtn: document.querySelector("[data-menu-close]"),
-    menu: document.querySelector("[data-menu]"),
-    body: document.querySelector("body"),
+    openMenuBtn: document.querySelector('[data-menu-open]'),
+    closeMenuBtn: document.querySelector('[data-menu-close]'),
+    menu: document.querySelector('[data-menu]'),
+    body: document.querySelector('body'),
+    menuLinks: document.querySelectorAll('.mobile-nav-menu-link'),
   };
 
-  refs.openMenuBtn.addEventListener("click", toggleModal);
-  refs.closeMenuBtn.addEventListener("click", toggleModal);
+  refs.openMenuBtn.addEventListener('click', toggleModal);
+  refs.closeMenuBtn.addEventListener('click', toggleModal);
+
+  refs.menuLinks.forEach((link) => {
+    link.addEventListener('click', () => {
+      toggleModal();
+    });
+  });
 
   function toggleModal() {
-    refs.menu.classList.toggle("is-hidden");
-    refs.body.classList.toggle("no-scroll");
+    refs.menu.classList.toggle('is-hidden');
+    refs.body.classList.toggle('no-scroll');
   }
 })();
